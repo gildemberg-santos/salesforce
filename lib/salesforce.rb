e3b741d72e8ce299b720afd6309814430e26a9dd
gildemberg-santos <gildemberg.santos@gmail.com>
2022-06-01 07:24:14 -0300

    Create OAuth class

--- lib/salesforce.rb
+++ lib/salesforce.rb
@@ -1,8 +1,38 @@
 # frozen_string_literal: true
 
 require_relative "salesforce/version"
+require 'uri'
+require 'json'
+require 'net/http'
 
 module Salesforce
   class Error < StandardError; end
-  # Your code goes here...
+
+  class OAuth
+    def initialize(client_id, client_secret, username, password, security_token,
+                   api_version = "v37.0")
+      @client_id = client_id
+      @client_secret = client_secret
+      @username = username
+      @password = password
+      @security_token = security_token
+      @api_version = api_version
+    end
+
+    def get_token
+      uri = URI.parse(oauth_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Post.new(uri.request_uri)
+      response = http.request(request)
+      JSON.parse(response.body)&.dig("access_token")
+    end
+
+    private
+
+    def oauth_url
+      "https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=#{@client_id}&client_secret=#{@client_secret}&username=#{@username}&password=#{@password}#{@security_token}"
+    end
+  end
 end
