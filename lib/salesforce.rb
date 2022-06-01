84791c19f599bb0c6c32bcee2b454b873b9d4144
gildemberg-santos <gildemberg.santos@gmail.com>
2022-06-01 07:22:53 -0300

    salesforce init

---
+++ lib/salesforce.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "salesforce/version"
+
+module Salesforce
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/salesforce/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Salesforce
+  VERSION = "0.1.0"
+end
