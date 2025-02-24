4bb9c63a41bfc44f3b9ed98d5789bc8b2c9a4bc4
gildemberg-santos <gildemberg.santos@gmail.com>
2025-02-23 21:40:42 -0300

    Adiciona uso de cassetes VCR nos testes da classe Salesforce::OAuth para simular chamadas à API e garantir a consistência dos testes.

--- spec/lib/salesforce/oauth_spec.rb
+++ spec/lib/salesforce/oauth_spec.rb
@@ -59,25 +59,29 @@ RSpec.describe Salesforce::OAuth do
     end.to raise_error(an_instance_of(Salesforce::Error).and(having_attributes(message: "Security token is required")))
   end
   it "Call method call" do
-    expect(Salesforce::OAuth.new(
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      username: USERNAME,
-      password: PASSWORD,
-      security_token: SECURITY_TOKEN
-    ).call).to be_nil
+    VCR.use_cassette("salesforce_oauth") do
+      expect(Salesforce::OAuth.new(
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        username: USERNAME,
+        password: PASSWORD,
+        security_token: SECURITY_TOKEN
+      ).call).to be_nil
+    end
   end
   it "Access token and Instance url" do
-    oauth = Salesforce::OAuth.new(
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      username: USERNAME,
-      password: PASSWORD,
-      security_token: SECURITY_TOKEN
-    )
-    oauth.call
+    VCR.use_cassette("salesforce_oauth") do
+      oauth = Salesforce::OAuth.new(
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        username: USERNAME,
+        password: PASSWORD,
+        security_token: SECURITY_TOKEN
+      )
+      oauth.call
 
-    expect(oauth.access_token).to_not be_nil
-    expect(oauth.instance_url).to_not be_nil
+      expect(oauth.access_token).to_not be_nil
+      expect(oauth.instance_url).to_not be_nil
+    end
   end
 end
